229ef0bc0370bba8fe16855dfe05278f7bd92108
mehdi <itemehdi97>
2024-04-13 14:38:38 +0330

    impl echo

---
+++ cmd/echo.go
@@ -0,0 +1,33 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"chaos/echo"
+
+	"github.com/spf13/cobra"
+)
+
+// echoCmd represents the echo command
+var echoCmd = &cobra.Command{
+	Use:   "echo",
+	Short: "A brief description of your command",
+	Run: func(cmd *cobra.Command, args []string) {
+		echo.Run()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(echoCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// echoCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// echoCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
